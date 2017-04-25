7519925e376ee5648f97619773bb67052c8cc11c
ITDS <ITDS@LENOVO-PC>
2017-04-25 02:35:00 -0500

    IndexThisExample

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        private class IndexerRecord
+
+        {
+            private readonly string[] data = new string[6];
+
+            private readonly string[] keys = {"Author", "Publisher", "Title", "Subject", "ISBN", "Comments"};
+
+            public string this[int idx]
+            {
+                set
+                {
+                     if (idx >= 0 && idx < data.Length)
+
+                        data[idx] = value;
+                }
+
+                get
+
+                {
+                    if (idx >= 0 && idx < data.Length)
+
+                        return data[idx];
+
+                    return null;
+                }
+            }
+
+            public string this[string key]
+
+            {
+                set
+
+                {
+                 //   var idx = FindKey(key);
+
+                 //    this[idx] = value;
+                }
+
+                get { return this[FindKey(key)]; }
+            }
+
+            private int FindKey(string key)
+
+            {
+                for (var i = 0; i < keys.Length; i++)
+
+                    if (keys[i] == key) return i;
+
+                return -1;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            var record = new IndexerRecord();
+
+            record[0] = "Wang xiaoping";
+
+            record[1] = "Springer";
+
+            record[2] = "Sea and old man";
+
+            Console.WriteLine(record["Title"]);
+
+            Console.WriteLine(record["Author"]);
+
+            Console.WriteLine(record["Publisher"]);
+            Console.WriteLine(record[0]);
+            Console.WriteLine(record[2]);
+        }
+    }
+}
---
+++ EventExampleIncrementer/EventExampleIncrementer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExampleIncrementer
+{
+    class Program
+    {
+        class IncrementerEventArgs : EventArgs                             // ****************publisher******************, sender 
+        {
+            public event EventHandler CountedADozen;      // delegate ,publish an Event   CounterADozen   
+
+            public void DoCount()                          //mothod  Docount to invoke event CounterADozen
+            {
+                for (int i = 1; i < 100;i++)
+                {
+                    if (i % 12 == 0 && CountedADozen !=null)   // check if the event null 
+                        CountedADozen(this, null);             // Invoke /fire/raise  Event  with parameters  , DozensCount fired when i= 12 
+                }
+
+            }
+        }
+
+        class  Dozons                                       //************ Subscriber****************  receiver 
+        {
+            public int DozensCount { get; private set; }
+
+            public Dozons(Incrementer incrementer)
+            {
+                DozensCount = 0;
+                incrementer.CountedADozen += IncrementDozenCount;   // Dozons subscribed a methode  to event 
+            }
+
+            void IncrementDozenCount(object source, EventArgs e)
+            {
+                DozensCount++;    //  claim Event program 
+            }
+        }
+        static void Main(string[] args)
+        {
+            Incrementer incrementer = new Incrementer();
+            
+            Dozons dozensCounter = new Dozons(incrementer);
+
+            incrementer.DoCount();
+            Console.WriteLine("number of dozens = {0}", dozensCounter.DozensCount);
+        }
+    }
+}
