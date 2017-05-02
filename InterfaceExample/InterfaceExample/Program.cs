5612ccd62d50664c5498997b81d35cf402211444
ITDS <ITDS@LENOVO-PC>
2017-05-02 17:43:48 -0500

    interface example2

--- InterfaceExample/InterfaceExample/Program.cs
+++ InterfaceExample/InterfaceExample/Program.cs
@@ -8,27 +8,73 @@ namespace InterfaceExample
 {
     class Program
     {
+        //----------------------------interface converter--------------------------
         interface IIfc1{void PrintOut(string s);}
         interface IIfc2{void PrintOut(string t);}
-
-        class  MyClass:IIfc1,IIfc2
+        class MyClass : IIfc1, IIfc2
         {
-            Void IIfc1.PrintOut(string s) {
-                Console.WriteLine("called through :{0}",s);
+            void IIfc1.PrintOut(string s)
+            {
+                Console.WriteLine("called through :{0}", s);
             }
-            Void IIfc2.PrintOut(string t)
+            void IIfc2.PrintOut(string t)
             {
                 Console.WriteLine("called through :{0}", t);
             }
             public void Method1()
             {
-                
+                ((IIfc1)this).PrintOut("sssss");
             }
 
-             
         }
+        //--------------------------------------- Interface animal example 
+        interface IliveBirth
+        {
+            void BabyCalled();
+        }
+
+        class  Animial 
+        {
+            
+        }
+
+        class cat : Animial, IliveBirth
+        {
+            void IliveBirth.BabyCalled()
+            {
+               Console.WriteLine(" Kitten called");
+            }
+        }
+
+        class  dog: Animial,IliveBirth
+        {
+            void IliveBirth.BabyCalled()
+            {
+                Console.WriteLine("puppy called");
+            }
+
+        }
+
+        class bird :Animial 
+        {
+            
+        }
+     
         static void Main(string[] args)
         {
+            Animial[]  animialArray=new Animial[3];
+            animialArray[0]=new cat();
+            animialArray[1]=new dog();
+            animialArray[2]=new bird();
+            foreach (Animial animial in animialArray)
+            {
+                IliveBirth b= animial as IliveBirth;
+                if (b != null)
+                {
+                    b.BabyCalled();
+                }
+                
+            }
         }
     }
 }
